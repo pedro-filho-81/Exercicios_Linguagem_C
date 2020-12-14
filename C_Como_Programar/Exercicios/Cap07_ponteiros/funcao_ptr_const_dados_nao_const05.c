7691cb4431a5a46f5f5d2ecb3f79d438a1e9f850
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2020-12-14 11:04:42 -0300

    Função Modificar
    Exemplo do livro C Como Programar dos Deitel.  Tenta modificar dados de uma variável  não constante por meio de um ponteiro constante.

---
+++ C_Como_Programar/Exercicios/Cap07_ponteiros/funcao_ptr_const_dados_nao_const05.c
@@ -0,0 +1,41 @@
+/*
+   Fig. 7.12: fig07_12.c
+   2 Tentando modificar dados por meio de um
+   3 ponteiro não constante para dados constantes.
+   Deitel, Paul; Deitel, Harvey. C  Como Programar (p. 218). Edição do Kindle.
+   Pedro Filho 14/12/2020
+*/
+
+// bibliotecas
+#include <stdio.h>
+#include <locale.h>
+
+// protótipo
+void f( const int *xPtr );
+
+int main()
+{
+   // aceita os caracteres do Português Brasil
+   setlocale( LC_ALL, "Portuguese" );
+
+   // variável
+   int y = 5;
+
+   // f tenta modificação ilegal
+   f( &y );
+
+   // pular uma linha
+   printf( "\n" );
+
+   return 0; // fim do programa
+
+}// fim main
+
+// função f tenta mudar um objeto
+void f( const char *xPtr )
+{
+   // xPtr não pode ser usado para modificar
+   // o valor da variável à qual ele aponta
+   *xPtr = 100; // não pode modificar o objeto const
+
+} // fim função
