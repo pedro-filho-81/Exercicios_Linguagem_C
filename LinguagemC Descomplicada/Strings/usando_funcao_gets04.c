3943c180f7ecad12bc9417ccc741f72297ff5e2e
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2020-12-22 16:44:39 -0300

    Lendo string II
    Programa mostra a leitura de string do teclado com a função gets( array ) ou com a função fgets( array, tamanho, stdin ) que limita o tamanho da string.

---
+++ LinguagemC Descomplicada/Strings/usando_funcao_gets04.c
@@ -0,0 +1,54 @@
+/*
+   Strings:  Usando a função gets()
+   Auto: Pedro Filho, 22/12/2020
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+#define TAMANHO 20
+
+// FUNÇÃO PRINCIPAL
+int main()
+{
+   // configura para português Brasil
+   setlocale( LC_ALL, "Portuguese" );
+
+   // cria um vetor para string
+   char frase[ TAMANHO ];
+
+   // ENtrada de dados
+   printf( "Digite uma frase: " );
+   // limpar o buffer, para ser usado com a função gets()
+   setbuf(stdin, NULL );
+   // entrada do usuário
+   gets( frase );
+
+   // mostrar o resultado
+   printf( "\nA melhor opção para a entrada de string pelo teclado \n"
+          "é usar a função gets() da biblioteca string.h");
+   printf( "\nA frase digitada foi: %s\n", frase );
+
+   // ENtrada de dados
+   printf( "Digite uma frase: " );
+   fflush( stdin );
+   // entrada do usuário
+   fgets( frase, 30, stdin );
+
+   // pular uma linha
+   printf( "\n" );
+
+   // mostrar o resultado
+   printf( "\nA melhor opção para a entrada de string pelo teclado \n"
+          "é usar a função fgets( nome do arrays, tamanho, stdin ) da biblioteca string.h\n");
+   printf( "\nA frase digitada foi: %s\n\n", frase );
+
+   // pausar
+   system( "pause" );
+
+   // fim do programa
+   return 0;
+
+}  // FIM FUNÇÃO MAIN
