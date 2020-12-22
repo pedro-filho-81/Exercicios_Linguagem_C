106d09b727ac261b9904974842d0a31652a56b7d
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2020-12-22 17:38:29 -0300

    Copiando uma String
    Programa passa a string do vetor A para o vetor B com strcpy( stringB, stringA ) stringB recebe os valores da stringA.

---
+++ LinguagemC Descomplicada/Strings/copiando_uma_string06.c
@@ -0,0 +1,51 @@
+/*
+   Strings:  copiando uma string a para uma string b
+   Auto: Pedro Filho, 22/12/2020
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+#define TAMANHO 50
+
+// FUNÇÃO PRINCIPAL
+int main()
+{
+   // configura para português Brasil
+   setlocale( LC_ALL, "Portuguese" );
+
+   // vetor para string
+   char texto1[ TAMANHO ];
+   char texto2[ TAMANHO ];
+
+   //entrada de dados
+   printf( "Digite uma frase: " );
+
+   // limpar o buffer
+   setbuf( stdin, NULL );
+
+   // ENTRADA DO USUÁRIO
+   gets( texto1 );
+
+   // mostrando string digitada
+   printf( "\nVocê digitou: %s\n", texto1 );
+
+   strcpy( texto2, texto1 );
+
+   // copiando e mostrando a string 2
+   printf( "\nEu sou a string2 que recebeu a cópia da string1"
+          "\ncom strcpy( string2, string1): \n%s\n", texto2 );
+
+
+   // pular uma linha
+   printf( "\n" );
+
+   // pausar
+   system( "pause" );
+
+   // fim do programa
+   return 0;
+
+}  // FIM FUNÇÃO MAIN
