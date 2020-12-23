86bc1c9799810298944ed7b867d3b7bbb5934e30
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2020-12-23 18:37:11 -0300

    Invertendo string II
    Programa para inverter um strinh usando a função strrev().

---
+++ LinguagemC Descomplicada/Strings/ExerciciosDeStrings/inverteCom_strrev04_1.c
@@ -0,0 +1,46 @@
+/*
+   Strings: invertendo string com a função strrev( string )
+   Auto: Pedro Filho, 23/12/2020
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+#define TAMANHO 30
+
+// FUNÇÃO PRINCIPAL
+int main()
+{
+   // configura para português Brasil
+   setlocale( LC_ALL, "Portuguese" );
+
+   // vetor para string
+   char texto[ TAMANHO ];
+
+   //entrada de dados
+   printf( "Digite uma frase: " );
+
+   // limpar o buffer
+   setbuf( stdin, NULL );
+
+   // ENTRADA DO USUÁRIO
+   gets( texto );
+
+   // mostrando a string
+   printf( "Você digitou: %s\n", texto );
+
+   // mostrar string invertida
+   printf( "A string invertida é %s\n", strrev( texto ) );
+
+   // pular uma linha
+   printf( "\n" );
+
+   // pausar
+   system( "pause" );
+
+   // fim do programa
+   return 0;
+
+}  // FIM FUNÇÃO MAIN
