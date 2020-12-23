02e70f7504e92b2438a1067540db61417380978f
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2020-12-23 15:54:37 -0300

    Update concatenando_string07.c
    Alterações

--- LinguagemC Descomplicada/Strings/concatenando_string07.c
+++ LinguagemC Descomplicada/Strings/concatenando_string07.c
@@ -5,6 +5,7 @@
 
 // biblioteca
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <locale.h>
 
@@ -15,27 +16,30 @@ int main () {
    setlocale( LC_ALL, "Portuguese" );
 
    // cria dois vetores fonte e destino
-   char fonte[50];
+   char origem[50];
    char destino[50];
 
    //strcpy copiar a string para o vetor fonte
-   strcpy(fonte,  "Eu sou a fonte ");
+   strcpy(origem,  "Eu sou a origem ");
 
    // mostrar a fonte
-   printf( "\nFonte: %s\n", fonte );
+   printf( "\nOrigem: %s\n", origem );
 
    // copiar a string para o destino
-   strcpy(destino, "Este é o destino <-- ");
+   strcpy(destino, "Eu sou o destino <-- ");
 
    // imprimir o destino
    printf( "\nDestino: %s\n", destino );
 
    // concatenar a fonte ao destino
    // coloca a string fonte depois da string destino
-   strcat(destino, fonte );
+   strcat( destino, origem );
 
    // mostrar o destino após a concatenação
    printf( "\nDestino apos concatenação:\n%s\n", destino  );
+
+   // pausar o sistema
+   system( "pause" );
 
    // fim programa
    return(0);
