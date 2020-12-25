84ddd1b5b70b1fdc6f19dd95608650df6e286b78
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2020-12-25 11:01:46 -0300

    Comparação entre strings
    programa usa a função da biblioteca C memcmp() para comparar strings.

---
+++ LinguagemC Descomplicada/Strings/BibliotecaString/compara_StringComString.c
@@ -0,0 +1,65 @@
+/*
+   Strings: A função da biblioteca C int memcmp
+   (const void * str1, const void * str2, size_t n))
+   compara os primeiros n bytes da área de memória
+   str1 e da área de memória str2
+   Auto: Pedro Filho, 24/12/2020
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+#define TAMANHO 20
+
+// FUNÇÃO PRINCIPAL
+int main()
+{
+   // configura para português Brasil
+   setlocale( LC_ALL, "Portuguese" );
+
+   // vetor para string
+   char texto1[ TAMANHO ];
+   char texto2[ TAMANHO ];
+   int resposta;
+
+   //entrada de dados
+   printf( "Digite uma ( frase, palavra ou texto ): " );
+   // limpar o buffer
+   setbuf( stdin, NULL );
+   // ENTRADA DO USUÁRIO
+   gets( texto1 );
+
+
+   //entrada de dados
+   printf( "Digite o caractere a ser pesquisado na string: " );
+   // limpar o buffer
+   setbuf( stdin, NULL );
+   // ENTRADA DO USUÁRIO
+   gets( texto2 );
+
+   // resposta recebe um valor da função memcmp()
+   resposta = memcmp( texto1, texto2, strlen( texto1 ) );
+
+   // verificar se resposta é diferente de null
+   if( resposta != NULL ) { // se diferente
+      // mostrar
+      printf( "O texto1 |%s| contem o texto2 [ %s ]\n", texto1, texto2 );
+   } // fim if resposta
+
+   else { // se igual a NULL
+      // imprima
+      printf( "O texto1 |%s|  não contem o texto2 [ %s ]\n", texto1, texto2 );
+   } // fim else resposta
+
+   // pular uma linha
+   printf( "\n" );
+
+   // pausar
+   system( "pause" );
+
+   // fim do programa
+   return 0;
+
+}  // FIM FUNÇÃO MAIN
