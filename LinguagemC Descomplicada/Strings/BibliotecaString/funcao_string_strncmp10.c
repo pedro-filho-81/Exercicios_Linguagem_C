aaf980bc960da1ce6f3cc5836d26322c05271563
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2020-12-27 16:55:48 -0300

    Função string strncmp
    compara no m�ximo os primeiros n bytes de str1 e str2 .

---
+++ LinguagemC Descomplicada/Strings/BibliotecaString/funcao_string_strncmp10.c
@@ -0,0 +1,71 @@
+/*
+   Strings: A função da biblioteca C int strncmp
+   (const char * str1, const char * str2, size_t n)
+   compara no máximo os primeiros n bytes de str1 e str2 .
+   Auto: Pedro Filho, 24/12/2020
+*/
+
+// biblioteca
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+#define TAMANHO 20
+
+// FUNÇÃO PRINCIPAL
+int main()
+{
+   // configura para português Brasil
+   setlocale( LC_ALL, "Portuguese" );
+
+   // vetor para string
+   char destDaPesquisa[ TAMANHO ];
+   char oriDaPesquisa[ TAMANHO ];
+   int resposta;
+
+   // cabeçalho
+   printf( "{ COMPARANDO STRING }\n" );
+
+   //entrada de dados
+   printf( "Digite uma ( frase, palavra ou texto ): " );
+   // limpar o buffer
+   setbuf( stdin, NULL );
+   // ENTRADA DO USUÁRIO
+   gets( destDaPesquisa );
+
+
+   //entrada de dados
+   printf( "Digite uma string a ser pesquisado na string anterior: " );
+   // limpar o buffer
+   setbuf( stdin, NULL );
+   // ENTRADA DO USUÁRIO
+   gets( oriDaPesquisa );
+
+   // resposta recebe um valor da função strcmp()
+   // STRNCMP( destino, origem )
+   resposta = strncmp( destDaPesquisa, oriDaPesquisa, strlen( destDaPesquisa ) );
+
+   // verificar se resposta é diferente de null
+   if( resposta != NULL ) { // se diferente
+      // mostrar
+      printf( "O texto1 |%s| contem o texto pesquisado [ %s ]\n",
+             destDaPesquisa, oriDaPesquisa );
+   } // fim if resposta
+
+   else { // se igual a NULL
+      // imprima
+      printf( "O texto1 |%s|  não contem o texto pesquisado [ %s ]\n",
+             destDaPesquisa, oriDaPesquisa );
+   } // fim else resposta
+
+   // pular uma linha
+   printf( "\n" );
+
+   // pausar
+   system( "pause" );
+
+   // fim do programa
+   return 0;
+
+}  // FIM FUNÇÃO MAIN
