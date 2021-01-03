058951799b08eea34c960c723c4c4bbbcf02230e
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-01-03 17:28:35 -0300

    Create enderecoDeCadaConteudo_array03.c

---
+++ LinguagemC Descomplicada/Ponteiros/Exercicios/enderecoDeCadaConteudo_array03.c
@@ -0,0 +1,28 @@
+/*
+   Ponteiros: 3) Crie um programa que contenha um array de float
+   com 10 elementos. Imprima o endereço de cada posição desse array.
+   Backes, André. Linguagem C (p. 213). GEN LTC. Edição do Kindle.
+   Auto: Pedro Filho, 03/01/2021
+*/
+
+// biblioteca
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// FUNÇÃO PRINCIPAL
+int main()
+{
+   // configura para português Brasil
+   setlocale( LC_ALL, "Portuguese" );
+
+   // pular uma linha
+   printf( "\n" );
+
+   // pausar
+   system( "pause" );
+
+   // fim do programa
+   return 0;
+
+}  // FIM FUNÇÃO MAIN
