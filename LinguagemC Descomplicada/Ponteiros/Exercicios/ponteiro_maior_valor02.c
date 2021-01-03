13d1495734806aff41a611b744dbea05887b3cc3
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-01-03 17:16:12 -0300

    Endereço e Conteúdo

---
+++ LinguagemC Descomplicada/Ponteiros/Exercicios/ponteiro_maior_valor02.c
@@ -0,0 +1,63 @@
+/*
+   Ponteiros: 2) Escreva um programa que contenha duas variáveis
+   inteiras. Leia essas variáveis do teclado. Em seguida, compare
+   seus endereços e exiba o conteúdo do maior endereço.
+   Backes, André. Linguagem C (p. 212). GEN LTC. Edição do Kindle.
+   Auto: Pedro Filho, 03/01/2021
+*/
+
+// biblioteca
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// FUNÇÃO PRINCIPAL
+int main()
+{
+   // configura para português Brasil
+   setlocale( LC_ALL, "Portuguese" );
+
+   //variáveis
+   int n1 = 5, n2 = 10;
+
+   // cria ponteiro *n1Ptr e recebe o endereço da variável n1
+   int *n1Ptr = &n1;
+
+   // cria ponteiro *n2Ptr e recebe o endereço da variável n2
+   int *n2Ptr = &n2;
+
+   // CABEÇALHO
+   printf( "{ MAIOR ENDEREÇO E O VALOR DA VARIÁVEL É }\n" );
+
+   // VERIFICA O MAIOR ENDEREÇO r mostrar conteúdo
+   // verifica se o endereço da variável n1 é maior que n2
+   // através dos ponteiros n1Ptr e n2Ptr
+   if( n1Ptr > n2Ptr )
+
+      // imprima que n1Ptr é o maior
+      printf( "O endereço da variável n1 = %d é maior que"
+             "\no endereço da variável n2 = %d\n"
+             "O CONTEÚDO da variável n1 é %d\n"
+             "o CONTEÚDO da variável n2 é %d\n",
+             n1Ptr, n2Ptr, *n1Ptr, *n2Ptr );
+
+   // se não
+   else
+
+      // imprima que n2Ptr é o maior
+      printf( "O endereço da variável n2 = %d é maior que"
+             "\no endereço da variável n1 = %d\n"
+             "O CONTEÚDO da variável n2 é %d\n"
+             "o CONTEÚDO da variável n1 é %d\n",
+             n2Ptr, n1Ptr, *n2Ptr, *n1Ptr );
+
+   // pular uma linha
+   printf( "\n" );
+
+   // pausar
+   system( "pause" );
+
+   // fim do programa
+   return 0;
+
+}  // FIM FUNÇÃO MAIN
