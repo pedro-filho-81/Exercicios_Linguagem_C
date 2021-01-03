442281b6c8fd8478d42feafbf6699f3dfc33c66f
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-01-03 11:04:41 -0300

    Matriz com Ponteiro
    Acessando matriz com ponteiro

---
+++ LinguagemC Descomplicada/Ponteiros/Aula/acessando_matriz_com_ponteiro04.c
@@ -0,0 +1,57 @@
+/*
+   Ponteiros:  Acessando matriz com ponteiro
+   Auto: Pedro Filho, 03/01/2021
+*/
+
+// biblioteca
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// FUNÇÃO PRINCIPAL
+int main()
+{
+   // configura para português Brasil
+   setlocale( LC_ALL, "Portuguese" );
+
+   // cria uma matriz 3 x 3
+   int num[ 3 ][ 3 ] = { {1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9, } };
+
+   // cria o ponteiro *numPtr que recebe
+   // o endereço da matriz num
+   int *numPtr = &num;
+
+
+   // imprime
+   printf( "Mostrando a matriz usando o próprio nome da matriz num[ i ][ j ]\nnum = " );
+
+   // loop para mostrar o array num
+   for( int i = 0; i < 3; i++ )
+   {
+      for( int j = 0; j < 3; j++ )
+      {
+         // imprime a matriz num usando a própria matriz
+         printf( "%d ", num[ i ][ j ] );
+      } // fim for interno
+   } // fim for
+
+   // imprime
+   printf( "\n\nMostrando a matriz usando o ponteiro *( numPtr + i ): \nnumPtr = " );
+
+   // loop para mostrar a matriz num com o ponteiro numPtr
+   for( int i = 0; i < 9; i++ )
+   {
+      // imprime a matriz num usando o ponteiro numPtr
+      printf( "%d ", *( numPtr + i ) );
+   } // fim for
+
+   // pular uma linha
+   printf( "\n\n" );
+
+   // pausar
+   system( "pause" );
+
+   // fim do programa
+   return 0;
+
+}  // FIM FUNÇÃO MAIN
