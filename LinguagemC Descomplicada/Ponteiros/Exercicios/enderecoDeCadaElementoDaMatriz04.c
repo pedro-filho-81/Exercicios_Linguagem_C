f015e9e07b6787a700bb62fc9fd8f0e509009915
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-01-04 10:33:20 -0300

    Update enderecoDeCadaElementoDaMatriz04.c
    Alterações

--- LinguagemC Descomplicada/Ponteiros/Exercicios/enderecoDeCadaElementoDaMatriz04.c
+++ LinguagemC Descomplicada/Ponteiros/Exercicios/enderecoDeCadaElementoDaMatriz04.c
@@ -3,7 +3,7 @@
    com três linhas e três colunas. Imprima o endereço de cada
    posição dessa matriz.
    Backes, André. Linguagem C (p. 213). GEN LTC. Edição do Kindle.
-   Auto: Pedro Filho, 02/01/2021
+   Auto: Pedro Filho, 04/01/2021
 */
 
 // biblioteca
@@ -14,49 +14,49 @@
 #define COLUNA 3
 
 // FUNÇÃO PRINCIPAL
-int main()
+int  main ()
 {
    // configura para português Brasil
-   setlocale( LC_ALL, "Portuguese" );
+   setlocale (LC_ALL, "Portuguese" );
 
    // cria matriz 3x3
-   float matriz[ LINHA ][ COLUNA ] = { 1.2, 2.11, 3.22, 4.34, 5.51, 6.28, 7.91, 8.45, 9.01, 10.87 };
+   float matriz[LINHA][COLUNA] = { 1.2, 2.11 , 3.22 , 4.34 , 5.51 , 6.28 , 7.91 , 8.45 , 9.01 , 10.87 };
 
    // CRIA O ponteiro que recebe o endereço da matriz
    float *mtzPtr = &matriz;
 
    // imprimir cabeçalho
-   printf( "\tMATRIZ 3 x 3\n" );
-   // loop para mostrar a matriz
-   for( int i = 0; i < LINHA; i++ )
+   printf ( "\tMATRIZ 3 x 3\n" );
+   // loop para mostrar uma matriz
+   for ( int i = 0; i < LINHA; i++)
    {
-      for( int j = 0; j < COLUNA; j++ )
+      for ( int j = 0 ; j < COLUNA; j++)
       {
          // imprimir os elementos da matriz usando o ponteiro
-         printf( "%7.2f", *( mtzPtr + i ) );
-      } // fim for
+         printf ( "%7.2f", *( mtzPtr + i ) );
+      } // fim para
 
       // pular linha
-      printf( "\n" );
-   } // fim for linha
+      printf ( "\n" );
+   } // fim para linha
 
-   printf( "\nValores e endereços da matriz.\n" );
+   printf ( "\nValores e endereços da matriz.\n" );
    // loop para mostrar os elementos e
    // endereços usando o ponteiro
-   for( int i = 0; i < LINHA * COLUNA; i++ )
+   for ( int i = 0; i < LINHA * COLUNA; i++)
    {
       // imprimir
-      printf( "O valor %5.2f está no endereço %d\n",
+      printf ( "O valor %5.2f está  no endereço %d\n" ,
              *( mtzPtr + i ), mtzPtr + i );
-   } // fim for
+   } // fim para
 
    // pular uma linha
-   printf( "\n" );
+   printf ( "\n" );
 
    // pausar
-   system( "pause" );
+   system ( "pause" );
 
    // fim do programa
-   return 0;
+   return  0 ;
 
-}  // FIM FUNÇÃO MAIN
+}   // FIM FUNÇÃO PRINCIPAL
