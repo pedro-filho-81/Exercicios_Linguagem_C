85fcbd5e499d8c37fba41e5a221aeb489315fa20
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-01-14 11:06:11 -0300

    Pre e Pós Incremento

---
+++ C_Como_Programar/Cap03/Exemplos/preEposIcremento04.c
@@ -0,0 +1,23 @@
+/*
+   Questão: Pre-incremento e Pós-incremento
+   Pedro Filho, 14/01/2021
+*/
+
+// Incluir Biblioteca
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// FUNÇÃO PRINCIPAL
+int main()
+{
+   // configura para português Brasil
+   setlocale( LC_ALL, "Portuguese" );
+
+   // pausar
+   system( "pause" );
+
+   // fim do programa
+   return 0;
+
+}  // FIM FUNÇÃO MAIN
