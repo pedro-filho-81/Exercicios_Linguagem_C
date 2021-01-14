089cbedf4d946d710a2b3e2d49f6964f988a887f
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-01-14 11:30:06 -0300

    Update preEposIcremento04.c
    Atualização

--- C_Como_Programar/Cap03/Exemplos/preEposIcremento04.c
+++ C_Como_Programar/Cap03/Exemplos/preEposIcremento04.c
@@ -14,6 +14,24 @@ int main()
    // configura para português Brasil
    setlocale( LC_ALL, "Portuguese" );
 
+   // variável
+   int c = 5;
+
+   // imprime pós-incremento
+   printf( "int c = 5; - cria variável\n" );
+   printf( "c = %d - variável antes do pós-incremento.\n", c );
+   printf( "c++ - pós-incremento soma 1 a c, "
+          "\nO valor de c depois do pós-incremento é %d\n", c++ );
+
+   // c recebe 5
+   c = 5;
+
+   // imprime pré-incremento
+   printf( "\nint c = 5; - cria variável\n" );
+   printf( "c = %d - variável antes do pré-incremento.\n", c );
+   printf( "++c - pré-incremento soma 1 a c "
+          "\nO valor de c depois do pré-incremento é %d\n\n", ++c );
+
    // pausar
    system( "pause" );
 
