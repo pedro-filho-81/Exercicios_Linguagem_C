8610df9a96548b95b1e6caab931cf3e2da2e07bc
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-01-17 09:17:07 -0300

    Pós e Pré-Incremento

---
+++ C_Como_Programar/Cap03/Exercicios/preIncrementoEPosIncremento3_22.c
@@ -0,0 +1,43 @@
+/*
+   Questão: 3.22 Pré-incrementando versus pós-incrementando.  Escreva um
+   programa que demonstre a diferença entre  pré-decrementar e pós-decrementar
+   usando o operador  de decremento --.
+   Deitel, Paul; Deitel, Harvey. C  Como Programar (p. 75). Edição do Kindle.
+   Autor: Pedro Filho, 17/01/2021
+*/
+
+// Incluir Biblioteca
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// FUNÇÃO PRINCIPAL
+int main()
+{
+   // configura para português Brasil
+   setlocale( LC_ALL, "Portuguese" );
+
+   // variável
+   int num = 10;
+
+   // imprime pré-Incremento
+   printf( "\tPRÉ-INCREMENTO PÓS-INCREMENTO\n" );
+   printf( "int num = 10 - cria a variável.\n" );
+   printf( "Imprime a variável num = %d\n", num );
+   printf( "Imprimindo variável usando o pré-incremento ( ++num ) num = %d\n", ++num );
+   printf( "Com o pré-incremento a variável começa com +1\n\n" );
+
+   // atribuindo 10 a num
+   num = 10;
+   // imprimindo
+   printf( "int num = 10 - cria a variável.\n" );
+   printf( "Imprime a variável num = %d\n", num );
+   printf( "imprimindo variável usando o pós-incremento ( num++ ) num = %d\n", num++ );
+   printf( "Com o pós-incremento a variável começa com o mesmo valor.\n\n" );
+   // pausar
+   system( "pause" );
+
+   // fim do programa
+   return 0;
+
+}  // FIM FUNÇÃO MAIN
