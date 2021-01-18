e92d8393a370eb183eb5a1e36ae3b63a056fcccb
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-01-18 09:42:13 -0300

    Create elsePendurado3_32.c

---
+++ C_Como_Programar/Cap03/Exercicios/elsePendurado3_32.c
@@ -0,0 +1,31 @@
+/*
+   Questão:
+   Autor: Pedro Filho, 15/01/2021
+*/
+
+// Incluir Biblioteca
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// Função principal
+int main()
+
+{ // inicio main
+    // variáveis
+    int x = 8;
+    int y = 5;
+
+    // condiç ão
+    if (y == 8){
+    if(x == 5){
+      printf("@@@@@\n");
+    } else {
+          printf("#####\n");
+          printf("$$$$$\n");
+          printf("&&&&&\n");
+       }
+    }
+
+	return 0; //Programa executado com sucesso.
+} // fim main
