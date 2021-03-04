15c4ee41ebbca7117e0eb2d32126411bbcabbe39
PedroFilho <psfilho81@hotmail.com>
2021-03-04 17:29:18 -0300

    Create somaElementosDoVetor6.6.c

---
+++ C_Como_Programar/Cap06-Array/Exemplos/somaElementosDoVetor6.6.c
@@ -0,0 +1,41 @@
+/*
+    Questão: /* Figura 6.6: fig06_06.c  2 Calcula a soma dos elementos do array
+    Deitel, Paul; Deitel, Harvey. C  Como Programar (p. 167). Edição do Kindle.
+    Pedro Filho, 04/03/2021
+*/
+
+// bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#define SIZE 12
+
+// função principal
+int main()
+{
+    // configura para português Brasil
+    setlocale( LC_ALL, "Portuguese" );
+
+    // variáveis
+    int a[ SIZE ] = { 1, 3, 5, 4, 7, 2, 99, 16, 45, 67, 89, 45 };
+    int i; // contador de for
+    int total = 0; // soma o vetor
+
+    //  loop para somar o vetor
+    for( i = 0; i < SIZE; i++ ) {
+        // soma o vetor
+        total += a[ i ];
+    } // fim for
+
+    printf( "O Total dos valores dos elementos do vetor é %d\n", total );
+
+    // pula uma linha
+    printf( "\n" );
+
+    // pausa o sistema
+    system( "pause" );
+
+    // termina o programa
+    return 0;
+
+} // fim função main
