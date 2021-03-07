faac14d2555853e605e4f1a5683088d5e828c7e0
PedroFilho <psfilho81@hotmail.com>
2021-03-07 16:07:04 -0300

    Create lancaDados6_9.c

---
+++ C_Como_Programar/Cap06-Array/Exemplos/lancaDados6_9.c
@@ -0,0 +1,55 @@
+/*
+    Questão: /* Figura 6.9: fig06_09.c  2 Lança um dado de 6 lados 6000 vezes
+    Deitel, Paul; Deitel, Harvey. C  Como Programar (p. 170). Edição do Kindle.
+    Autor: Pedro Filho, 07/03/2021
+*/
+
+// bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+#define SIZE 7
+
+// função principal
+int main()
+{
+    // configura para português Brasil
+    setlocale( LC_ALL, "Portuguese" );
+
+    // variáveis
+    int face; // valor aleatório de 1 a 6
+    int rola; // contador de 1 a 6000
+    int frequencia[ SIZE ] = {0}; // contador da frequência
+
+    // gerador
+    srand( time( NULL ) );
+
+    // LOOP PARA rolar o dado
+    for( rola = 1; rola <= 6000; rola++ )
+    {
+        // gera um número aleatório entre 1 e 6
+        face =  1 + rand() % 6;
+        // conta com que frequência um número da face aparece
+        ++frequencia[ face ];
+    }
+
+    // imprime cabeçalho
+    printf( "%s%13s\n", "Face", "Frequência" );
+
+    // loop para mostrar a frequência
+    for( face = 1; face < SIZE; face++ )
+    {
+        printf( "%3d%10d\n", face, frequencia[ face ] );
+    } // fim for
+
+    // pula uma linha
+    printf( "\n" );
+
+    // pausa o sistema
+    system( "pause" );
+
+    // termina o programa
+    return 0;
+
+} // fim função main
