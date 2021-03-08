cb5f0a3ec5fff82966c81d67e280fd8049a47ad4
PedroFilho <psfilho81@hotmail.com>
2021-03-08 09:57:27 -0300

    Create arraysEstaticos6_11.c

---
+++ C_Como_Programar/Cap06-Array/Exemplos/arraysEstaticos6_11.c
@@ -0,0 +1,90 @@
+/*
+    Questão: /* Figura 6.11: fig06_11.c  2 Arrays estáticos são inicializados em zero
+    Deitel, Paul; Deitel, Harvey. C  Como Programar (p. 173). Edição do Kindle.
+    Autor: Pedro Filho, 08/03/2021
+*/
+
+// bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// protótipos
+void automaticoArrayInit();
+void estaticoArrayInit();
+
+// função principal
+int main()
+{
+    // configura para português Brasil
+    setlocale( LC_ALL, "Portuguese" );
+
+    // variáveis
+
+    // imprime
+    printf( "Primeira chamada para cada função:\n" );
+    automaticoArrayInit();
+    estaticoArrayInit();
+
+    // imprime
+    printf( "\n\nSegunda chamada paracada função:\n");
+    automaticoArrayInit();
+    estaticoArrayInit();
+
+    // pula uma linha
+    printf( "\n" );
+
+    // pausa o sistema
+    system( "pause" );
+
+    // termina o programa
+    return 0;
+
+} // fim função main
+
+// função estaticoArrayInit
+void estaticoArrayInit()
+{
+    static int array1[ 3 ];
+    int i;
+
+    printf( "\nValores na entrada de estaticpArrayInit()\n" );
+
+    // loop for
+    for( i = 0; i < 3; i++ )
+    {
+        printf( "\narray[ %d ] = %d ", i, array1[ i ] );
+    } // fim for
+
+    printf( "\nValores na saída de estaticpArrayInit()\n" );
+
+    // loop for
+    for( i = 0; i < 3; i++ )
+    {
+        printf( "\narray[ %d ] = %d ", i, array1[ i ] += 5 );
+    } // fim for
+
+} // fim função
+
+void automaticoArrayInit()
+{
+
+    static int array1[ 3 ] = {1, 2, 3};
+    int i;
+
+    printf( "\n\nValores na entrada de automaticoArrayInit()" );
+
+    // loop for
+    for( i = 0; i < 3; i++ )
+    {
+        printf( "\narray[ %d ] = %d ", i, array1[ i ] );
+    } // fim for
+
+    printf( "\nValores na saída de automaticoArrayInit()" );
+
+    // loop for
+    for( i = 0; i < 3; i++ )
+    {
+        printf( "\narray[ %d ] = %d ", i, array1[ i ] += 5 );
+    } // fim for
+}
