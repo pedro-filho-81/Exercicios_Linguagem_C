a73c16eb157f9ce1a3b59fdb14cb6434f6e1e7c8
PedroFilho <psfilho81@hotmail.com>
2021-03-08 10:24:53 -0300

    Create arrayComMesmoEndereco6_12.c

---
+++ C_Como_Programar/Cap06-Array/Exemplos/arrayComMesmoEndereco6_12.c
@@ -0,0 +1,33 @@
+/*
+    Questão: array com mesmo endereço
+    Autor: Pedro Filho, 08/03/2021
+*/
+
+// bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// função principal
+int main()
+{
+    // configura para português Brasil
+    setlocale( LC_ALL, "Portuguese" );
+
+    // variáveis
+    int array[ 5 ]; // array com 5 elementos
+
+    printf( "Arrays diferentes com o mesmo endereço\n" );
+    printf( "array = %p\n&array[ 0 ] = %p\n&array = %p\n",
+           array, &array[ 0 ], &array );
+
+    // pula uma linha
+    printf( "\n" );
+
+    // pausa o sistema
+    system( "pause" );
+
+    // termina o programa
+    return 0;
+
+} // fim função main
