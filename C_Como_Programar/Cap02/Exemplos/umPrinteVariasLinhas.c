47829625982c550bae86fcc32a8eccd09d250aee
PedroFilho <psfilho81@hotmail.com>
2021-08-16 19:26:21 -0300

    Um Printf várias Linhas

---
+++ C_Como_Programar/Cap02/Exemplos/umPrinteVariasLinhas.c
@@ -0,0 +1,14 @@
+/*
+    /* Figura 2.4: fig02_04.c
+    2 Imprimindo várias linhas com uma única instrução printf
+    Deitel, Paul; Deitel, Harvey. C  Como Programar (p. 23). Edição do Kindle.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf( "Bem-vindo\na\nC!\n" );
+
+    return 0; // fim do programa
+} // fim de main
