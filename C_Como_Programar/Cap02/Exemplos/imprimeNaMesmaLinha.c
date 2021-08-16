1e09eca4aaac3929863ace6a139b2ffa24c4c201
PedroFilho <psfilho81@hotmail.com>
2021-08-16 19:19:57 -0300

    Imprimindo uma linha

---
+++ C_Como_Programar/Cap02/Exemplos/imprimeNaMesmaLinha.c
@@ -0,0 +1,17 @@
+/*
+    /* Figura 2.3: fig02_03.c
+    2 Imprimindo em uma linha com duas instruções printf
+    Deitel, Paul; Deitel, Harvey. C  Como Programar (p. 23). Edição do Kindle.
+    Autor:
+*/
+
+#include <stdio.h>
+
+// cria função principal
+int main( void )
+{
+    printf( "Bem-vindo " );
+    printf( "a C!\n");
+
+    return 0; // fim do programa
+} // fim main
