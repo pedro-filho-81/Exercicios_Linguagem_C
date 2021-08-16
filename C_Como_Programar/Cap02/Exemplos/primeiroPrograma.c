ad2f8e587f0a9159856a38f5461e3ee63cc34c69
PedroFilho <psfilho81@hotmail.com>
2021-08-16 19:12:36 -0300

    Primeiro Programa

---
+++ C_Como_Programar/Cap02/Exemplos/primeiroPrograma.c
@@ -0,0 +1,17 @@
+/*
+    1Figura 2.1: fig02_01.c
+    2 Primeiro programa em C
+    Deitel, Paul; Deitel, Harvey. C  Como Programar (p. 20). Edição do Kindle.
+    Autor:
+*/
+#include <stdio.h>
+
+// função principal
+int main()
+{
+    // imprima
+    printf("Bem vindo a linguagem C!" );
+
+    // fim programa
+    return 0;
+} // fim main
