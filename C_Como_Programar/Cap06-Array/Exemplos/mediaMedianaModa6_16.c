dbb91dc403708a3f6e40856cbef9d556348d8e4d
PedroFilho <psfilho81@hotmail.com>
2021-10-17 10:57:01 -0300

    Atualização

--- C_Como_Programar/Cap06-Array/Exemplos/mediaMedianaModa6_16.c
+++ C_Como_Programar/Cap06-Array/Exemplos/mediaMedianaModa6_16.c
@@ -54,6 +54,9 @@ int main()
     // chama a função mediana
     median( response, SIZE );
 
+    // chama a função mode
+    mode( frequency, FREQ_SIZE, response, SIZE );
+
     printf( "\n" ); // pula linha
 
     system("pause"); // pausa do programa
@@ -79,7 +82,7 @@ void printArray(const int vetor[], int size )
     for( int mostrar = 0; mostrar < size; mostrar++ )
     {
         // se mostrar for multiplo de 10
-        if( mostrar % 10 == 0 )
+        if( mostrar % 20 == 0 )
             // pula linha
             printf("\n");
 
@@ -135,8 +138,73 @@ void median( int answer[], int size )
               "do array ordenado de %d elementos.\n"
               "Para essa execução, a mediana é %d\n\n",
               size / 2, size, answer[ size / 2] );
-
 } // fim função median
+
+// cria a função moda
+void mode(int freq[], int freqSize, const int answer[], int size )
+{
+    // variáveis
+    int rating; // contador para acessar os elementos de 1-9 do array freq
+    int j; // contador para resumir os elementos de 0-98 do array answer
+    int h; // contador para exibir histograma dos elementos do array freq
+    int largest = 0; // representa a maior frequência
+    int modeValue = 0; // representa resposta mais frequente
+
+    // cabeçal
+    printf( "%s\n%s\n%s\n", "********************", "       Moda", "********************" );
+
+    // inicializa frequência em zero
+    for( int rating = 0; rating < freqSize; rating++ )
+    {
+        // frequência recebe zero
+        freq[ rating ] = 0;
+    } // fim for rating
+
+    // frequência de resumo
+    for( j = 0; j < size; j++ )
+    {
+        // mostrar
+        ++freq[ answer[ j ]  ];
+    } // fim for
+
+    // cabeçalho
+    printf( "%s%15s%19s\n",
+               "Resposta", "Frequência", "Histrograma");
+
+    // loop mostra frequência
+    for( rating = 1; rating < freqSize; rating++ )
+    {
+       // imprima
+       printf( "%6d%16d", rating, freq[ rating ] );
+
+       // acompanha o valor da moda e da maior frequência
+       if( freq[ rating ] > largest )
+       {
+           // largest recebe freq
+           largest = freq[ rating ];
+           // modeValue recebe rating
+           modeValue = rating;
+       } // fim if
+
+       printf( "         " ); // imprime 9 espaços em brando
+
+       // loop para mostrar a barra de asterisco ( histograma )
+       for( int asterisco = 1; asterisco <= freq[ rating ]; asterisco++ )
+       {
+           // imprime assterisco
+           printf( "*" );
+       } // fim for asterisco
+
+       // pula linha
+       printf( "\n" );
+    } // fim for mostra frequência
+
+    // imprime o valor da moda
+    printf( "A moda é o valor mais frequênte.\n"
+               "Para essa execução a moda é %d que ocorreu"
+               " %d vezes.\n", modeValue, largest );
+
+} // fim função mode
 
 // cria a função bubbleSort
 void bubbleSort( int vetor[], int size )
@@ -154,7 +222,6 @@ void bubbleSort( int vetor[], int size )
                 auxiliar = vetor[ j ];
                 vetor[ j ] = vetor[ i ];
                 vetor[ i ] = auxiliar;
-
             } // fim if
         } // fim for interno
     } // fim for externo
