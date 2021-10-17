1d310ce9960ca94e50d57518463446125c6987c0
PedroFilho <psfilho81@hotmail.com>
2021-10-17 18:10:18 -0300

    Pesquisa Binária

---
+++ C_Como_Programar/Cap06-Array/Exemplos/pesquisaBinaria6_19.c
@@ -0,0 +1,28 @@
+/*
+    1 Figura 6.19: fig06_19.c
+    2 Pesquisa binária de um array ordenado
+    Deitel, Paul; Deitel, Harvey. C  Como Programar (p. 186). Edição do Kindle.
+    17/10/2021
+*/
+
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 100
+
+// função principal
+int main()
+{
+    // localização geográfica
+    setlocale( LC_ALL, "portuguese");
+
+    // limpa a tela
+    system("cls");
+
+    system("pause"); // pausa do programa
+
+    return 0; // programa terminado com sucesso
+
+} // fim main
