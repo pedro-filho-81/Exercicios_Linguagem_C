38f7cd57b993ef8d06a72554a814831d5cb366c3
PedroFilho <psfilho81@hotmail.com>
2024-07-30 12:06:26 -0300

    Update formasComAsterisco23.c

--- C_Como_Programar/Cap02/Exercicios/formasComAsterisco23.c
+++ C_Como_Programar/Cap02/Exercicios/formasComAsterisco23.c
@@ -1,3 +1,8 @@
+/*
+    2.21 Formas com asteriscos. Escreva um programa que  imprima as seguintes formas com asteriscos. 
+    Deitel, Paul; Deitel, Harvey. C Como Programar (Portuguese Edition) (p. 42). Edição do Kindle. 
+    Pedro Filho, 30/07/2024
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
@@ -12,6 +17,7 @@ int main() {
     printf("*       *   *     *    *       *   *\n");
     printf("*       *    *   *     *        * *\n");
     printf("*********     ***      *         *\n");
-    
+
     return 0;
+
 } // end main
