7a2af49df199cbd32febb04904a12c75ba9d0416
PedroFilho <psfilho81@hotmail.com>
2024-07-30 12:06:17 -0300

    Update base.c

--- C_Como_Programar/baseC/base.c
+++ C_Como_Programar/baseC/base.c
@@ -3,5 +3,12 @@
 #include <locale.h>
 int main() {
 
+    // configura para o português do brasil
+    setlocale(LC_ALL, "portuguese");
+
+    // seu código aqui
+
+    system("pause"); // pausa a execução
     return 0;
+    
 } // end main
