9212e506f9553423f813f9ac9654013ddf32b74b
PedroFilho <psfilho81@hotmail.com>
2024-07-30 11:45:16 -0300

    Create formasComAsterisco23.c

---
+++ C_Como_Programar/Cap02/Exercicios/formasComAsterisco23.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main() {
+
+    printf("*********     ***      *         *\n");
+    printf("*       *    *   *    ***       * *\n");
+    printf("*       *   *     *  *****     *   *\n");
+    printf("*       *   *     *    *      *     *\n");
+    printf("*       *   *     *    *     *       *\n");
+    printf("*       *   *     *    *      *     *\n");
+    printf("*       *   *     *    *       *   *\n");
+    printf("*       *    *   *     *        * *\n");
+    printf("*********     ***      *         *\n");
+    
+    return 0;
+} // end main
